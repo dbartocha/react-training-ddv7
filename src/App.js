1ac1375d15e7fc7f6bfdeac4b57eb59dc5374d29
Bartocha <dbartocha@deloittece.com>
2022-01-10 16:13:03 +0100

    App updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { useState } from "react";
+import Cart from "./components/Cart/Cart";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
+
 function App() {
+
+  const [isOverlay, setIsOverlay] = useState(false);
+
+  const showCart = () => {
+    setIsOverlay(true);
+  }
+
+  const hideCart = () => {
+    setIsOverlay(false);
+  }
+
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+      <CartProvider>
+        {isOverlay && <Cart onHide={hideCart}/>}
+        <Header  onShow={showCart}/>
+        <main>
+          <Meals/>
+        </main>
+      </CartProvider>
   );
 }
 
